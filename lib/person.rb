6c45ae3cd37bec451c4314967e215e4505c59535
Jorge Ramirez <joramirez96@gmail.com>
2020-06-30 19:08:08 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,10 +1,13 @@
 class Person
   #your code here
+  avi_attributes = {name, birthday, hair_color, eye_color, height,
+weight, handed, complexion, t_shirt_size,
+wrist_size, glove_size, pant_length, pant_width}
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
 :weight, :handed, :complexion, :t_shirt_size,
 :wrist_size, :glove_size, :pant_length, :pant_width
 
+
   def initialize
-    @name = name
-    @birthday = birthday
+    @avi_attributes = avi_attributes
 end
